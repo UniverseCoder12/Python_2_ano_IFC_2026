d27b7d0fee7f528420ae11215c6957fe14318302
UniverseCoder12 <166874928+UniverseCoder12@users.noreply.github.com>
2026-03-25 12:01:49 +0000

    gh

--- Exercicios/oficina_correção.py
+++ Exercicios/oficina_correção.py
@@ -2,7 +2,22 @@ class Veiculo:
    def __init__(self, placa, cor):
       self.placa = placa
       self.cor = cor
+   
+   def __str__(self):
+      return f'''
+Placa: {self.placa}
+Cor: {self.cor}'''
 
 class Carro(Veiculo):
-   def __init__(self, por):
+   def __init__(self, por, placa, cor):
       super().__init__(placa, cor)
+      self.porta = por
+   
+   def __str__(self):
+      return f'''
+{super().__str__()}
+portas: {self.porta}
+              '''
+
+a = Carro(2, "fwf-hehe", "azul")
+print(a)
